c61a9089ed4a9244676c205a33a1a86552680330
EngTaqi <taqihacko@gmail.com>
2020-12-01 22:13:11 +0300

    init

--- functions.py
+++ functions.py
@@ -20,7 +20,7 @@ print(mean(1,2,3,4))
 
 
 #a function with keyword agrumenta the mean of any number of arguments
-def mean(**kwargs):
-    return kwargs
-print(mean(a=1,b=2,c=1,d=4))
+def find_winner(**kwargs):
+    return max(kwargs, key = kwargs.get)
 
+print(find_winner(Andy = 17, Marry = 19, Sim = 45, Kae = 34))
