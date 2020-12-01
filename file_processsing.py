b63e21eb801e21b0cc19ec56098c6b80212f1a4c
EngTaqi <taqihacko@gmail.com>
2020-12-01 23:28:29 +0300

    init

--- file_processsing.py
+++ file_processsing.py
@@ -1,3 +1,24 @@
 #to read file 
 myfile = open("fruits.txt","r")
-print(myfile.read())
+print(myfile.read()) #read the file but the cursor at last row
+#read file multi times
+content=myfile.read()
+print(content)
+print(content)
+print(content)
+#you must colse your file
+myfile.close()
+
+#the best way to open a file
+with open('fruits.txt') as myfile2:
+    content2=myfile2.read
+#the close here is auto
+#write a file
+with open('fruits.txt','w') as myfile2:
+    myfile2.write('pear\napple\norange\nmandarin\nwatermelon\npomegranate\ntommato')
+#append
+with open('fruits.txt','a') as myfile2:
+    myfile2.write('\nbanana')
+#a+ for read and append
+myfile.seek(0)
+#to make the coursor at zero possition
