af29d9b958edb9c9a295255454651cc02d59ae6c
EngTaqi <taqihacko@gmail.com>
2020-12-02 11:32:08 +0300

    init

--- modules.py
+++ modules.py
@@ -7,4 +7,16 @@ time.sleep(5) #lag time 5 sec
 while True:
     with open("fruits.txt") as file:
         file.read()
-        time.sleep(10)
+        time.sleep(2)
+#if my file dosenot exist but i want to keep running
+import os
+os.path.exists("fruits") #gives ture
+while True:
+    time.sleep(2)
+    if os.path.exists("fruits"):
+    with open("fruits.txt") as file:
+        file.read()
+        time.sleep(2)
+    else:
+        print("file doesnot exist")
+import pandas
